a548acfefc7649f043fac6e0dd02386566d2c883
David Piepgrass <qwertie256@gmail.com>
2016-05-15 13:11:44 +0800

    SetOrCreateMember macro: refactored; added logic to support constructors that call other constructors.

--- Main/LeMP.StdMacros/SetOrCreateMemberMacro.cs
+++ Main/LeMP.StdMacros/SetOrCreateMemberMacro.cs
@@ -28,93 +28,69 @@ namespace LeMP
 			if (fn.ArgCount < 3 || !fn.Args[2].Calls(S.AltList))
 				return null;
 			var args = fn.Args[2].Args;
-			LNode body = null;
 
 			VList<LNode> propOrFieldDecls = VList<LNode>.Empty;
-			VList<LNode> setStmts = VList<LNode>.Empty;
+			Dictionary<Symbol, LNode> assignments = null;
 			for (int i = 0; i < args.Count; i++) {
 				var arg = args[i];
-				Symbol a = S.Property;
-				Symbol fieldName = null;
-				Symbol paramName = null;
-				LNode plainArg = null;
-				LNode propOrFieldDecl = null;
-				if (arg.CallsMin(S.Property, 4)) {
-					// #property(Type, Name<T>, {...})
-					var name = arg.Args[1];
-					fieldName = EcsNodePrinter.KeyNameComponentOf(name);
-					paramName = ChooseArgName(fieldName);
-					if (arg.ArgCount == 5) { // initializer is Args[4]
-						plainArg = F.Var(arg.Args[0], F.Call(S.Assign, F.Id(paramName), arg.Args[4]));
-						propOrFieldDecl = arg.WithArgs(arg.Args.First(4));
-					} else {
-						plainArg = F.Var(arg.Args[0], paramName);
-						propOrFieldDecl = arg;
-					}
-				} else {
-					LNode type, defaultValue;
-					if (IsVar(arg, out type, out paramName, out defaultValue)) {
-						int a_i = 0;
-						foreach (var attr in arg.Attrs) {
-							if (attr.IsId) {
-								a = attr.Name;
-								if (a == _set 
-									|| a == S.Public || a == S.Internal || a == S.Protected || a == S.Private
-									|| a == S.ProtectedIn || a == S.Static || a == S.Partial)
-								{
-									fieldName = paramName;
-									paramName = ChooseArgName(fieldName);
-									if (a == _set) {
-										plainArg = F.Var(type, paramName, defaultValue).WithAttrs(arg.Attrs.RemoveAt(a_i));
-									} else {
-										// in case of something like "[A] public params T arg = value", 
-										// assume that "= value" represents a default value, not a field 
-										// initializer, that [A] belongs on the field, except `params` 
-										// which stays on the argument.
-										plainArg = F.Var(type, paramName, defaultValue);
-										propOrFieldDecl = arg;
-										if (arg.Args[1].Calls(S.Assign, 2))
-											propOrFieldDecl = arg.WithArgChanged(1,
-												arg.Args[1].Args[0]);
-										int i_params = arg.Attrs.IndexWithName(S.Params);
-										if (i_params > -1)
-										{
-											plainArg = plainArg.PlusAttr(arg.Attrs[i_params]);
-											propOrFieldDecl = propOrFieldDecl.WithAttrs(propOrFieldDecl.Attrs.RemoveAt(i_params));
-										}
-									}
-									break;
-								}
-							}
-							a_i++;
-						}
-					}
-				}
-				if (plainArg != null)
+				Symbol relevantAttribute, fieldName, paramName;
+				LNode plainArg, propOrFieldDecl;
+				if (DetectSetOrCreateMember(arg, out relevantAttribute, out fieldName, out paramName, out plainArg, out propOrFieldDecl))
 				{
-					if (body == null)
-					{
-						if (fn.ArgCount < 4 || !fn.Args[3].Calls(S.Braces))
-							return Reject(sink, arg, Localize.Localized("'{0}': to set or create a field or property, the method must have a body in braces {{}}.", a));
-						body = fn.Args[3];
-					}
+					if (fn.ArgCount < 4)
+						return Reject(sink, arg, Localize.Localized("'{0}': to set or create a field or property, the method must have a body in braces {{}}.", relevantAttribute));
 
 					args[i] = plainArg;
-					LNode assignment;
-					if (fieldName == paramName)
-						assignment = F.Call(S.Assign, F.Dot(F.@this, F.Id(fieldName)), F.Id(paramName));
-					else
-						assignment = F.Call(S.Assign, F.Id(fieldName), F.Id(paramName));
-					setStmts.Add(assignment);
+					assignments = assignments ?? new Dictionary<Symbol, LNode>();
+					assignments[fieldName] = F.Id(paramName);
 					if (propOrFieldDecl != null)
 						propOrFieldDecls.Add(propOrFieldDecl);
 				}
 			}
-			if (body != null) // if this macro has been used...
+
+			if (assignments != null) // if this macro has been used...
 			{
 				var parts = fn.Args;
 				parts[2] = parts[2].WithArgs(args);
-				parts[3] = body.WithArgs(body.Args.InsertRange(0, setStmts));
+				var body = parts[3];
+				
+				// Ensure the method has a normal braced body
+				if (!body.Calls(S.Braces)) {
+					if (parts[0].IsIdNamed(S.Void))
+						body = F.Braces(body);
+					else
+						body = F.Braces(F.Call(S.Return, body));
+				}
+
+				// In case one constructor calls another, we have to ensure that the 
+				// assignments are inserted _after_ that call, and if the constructor
+				// call refers to properties or fields that will be set, we must remap
+				// those references onto parameters, e.g.
+				//   this(public int X) { base(X); } => this(int x) { base(x); X = x; }
+				var bodyStmts = body.Args;
+				int indexAtWhichToDoAssignments = 0;
+				if (fn.Calls(S.Constructor)) {
+					LNode baseCall = bodyStmts[0, LNode.Missing];
+					if (baseCall.Calls(S.Base) || baseCall.Calls(S.This)) {
+						bodyStmts[0] = baseCall.ReplaceRecursive(n => {
+							LNode param;
+							if (n.IsId && assignments.TryGetValue(n.Name, out param))
+								return param;
+							return null;
+						});
+						indexAtWhichToDoAssignments = 1;
+					}
+				}
+
+				// Insert assignment statements
+				parts[3] = body.WithArgs(bodyStmts.InsertRange(indexAtWhichToDoAssignments, assignments.Select(p => {
+					if (p.Key == p.Value.Name)
+						return F.Call(S.Assign, F.Dot(F.@this, F.Id(p.Key)), p.Value);
+					else
+						return F.Call(S.Assign, F.Id(p.Key), p.Value);
+				}).ToList()));
+
+				// Return output code
 				fn = fn.WithArgs(parts);
 				if (propOrFieldDecls.IsEmpty)
 					return fn;
@@ -126,23 +102,74 @@ namespace LeMP
 			return null;
 		}
 
-		private static bool IsVar(LNode arg,out LNode type, out Symbol name, out LNode defaultValue)
+		private static bool DetectSetOrCreateMember(LNode arg, out Symbol relevantAttribute, out Symbol fieldName, out Symbol paramName, out LNode plainArg, out LNode propOrFieldDecl)
 		{
-			type = defaultValue = null;
-			name = null;
- 			if (arg.Calls(S.Var, 2)) {
-				type = arg.Args[0];
-				var n = arg.Args[1];
-				if (n.IsId) {
-					name = n.Name;
-					return true;
-				} else if (n.Calls(S.Assign, 2) && n.Args[0].IsId) {
-					name = n.Args[0].Name;
-					defaultValue = n.Args[1];
-					return true;
+			relevantAttribute = null;
+			fieldName = null;
+			paramName = null;
+			plainArg = null;
+			propOrFieldDecl = null;
+			LNode _, type, name, defaultValue, propArgs;
+			if (EcsValidators.IsPropertyDefinition(arg, out type, out name, out propArgs, out _, out defaultValue) && propArgs.ArgCount == 0) {
+				// #property(Type, Name<T>, {...})
+				relevantAttribute = S.Property;
+				fieldName = EcsNodePrinter.KeyNameComponentOf(name);
+				paramName = ChooseArgName(fieldName);
+				if (defaultValue != null) { // initializer is Args[4]
+					plainArg = F.Var(type, paramName, defaultValue);
+					propOrFieldDecl = arg.WithArgs(arg.Args.First(4));
+				} else {
+					plainArg = F.Var(type, paramName);
+					propOrFieldDecl = arg;
 				}
+				return true;
+			} else if (IsVar(arg, out type, out paramName, out defaultValue)) {
+				int a_i = 0;
+				foreach (var attr in arg.Attrs) {
+					if (attr.IsId) {
+						var a = attr.Name;
+						if (a == _set
+							|| a == S.Public || a == S.Internal || a == S.Protected || a == S.Private
+							|| a == S.ProtectedIn || a == S.Static || a == S.Partial) {
+							relevantAttribute = a;
+							fieldName = paramName;
+							paramName = ChooseArgName(fieldName);
+							if (a == _set) {
+								plainArg = F.Var(type, paramName, defaultValue).WithAttrs(arg.Attrs.Without(attr));
+							} else {
+								// in case of something like "[A] public params T arg = value", 
+								// assume that "= value" represents a default value, not a field 
+								// initializer, that [A] belongs on the field, except `params` 
+								// which stays on the argument.
+								plainArg = F.Var(type, paramName, defaultValue);
+								propOrFieldDecl = arg;
+								if (arg.Args[1].Calls(S.Assign, 2))
+									propOrFieldDecl = arg.WithArgChanged(1,
+										arg.Args[1].Args[0]);
+								int i_params = arg.Attrs.IndexWithName(S.Params);
+								if (i_params > -1) {
+									plainArg = plainArg.PlusAttr(arg.Attrs[i_params]);
+									propOrFieldDecl = propOrFieldDecl.WithAttrs(propOrFieldDecl.Attrs.RemoveAt(i_params));
+								}
+							}
+							break;
+						}
+					}
+					a_i++;
+				}
+				return plainArg != null;
 			}
 			return false;
+		}
+
+		private static bool IsVar(LNode arg,out LNode type, out Symbol name, out LNode defaultValue)
+		{
+			name = null;
+			LNode nameNode;
+			if (!EcsValidators.IsVariableDeclExpr(arg, out type, out nameNode, out defaultValue))
+				return false;
+			name = nameNode.Name;
+			return nameNode.IsId;
 		} 
 		static Symbol ChooseArgName(Symbol fieldName)
 		{
--- Main/LeMP/Tests/TestSetOrCreateMemberMacro.cs
+++ Main/LeMP/Tests/TestSetOrCreateMemberMacro.cs
@@ -11,12 +11,8 @@ namespace LeMP.Tests
 	public class TestSetOrCreateMemberMacro : MacroTesterBase
 	{
 		[Test]
-		public void SetOrCreateMemberTest()
+		public void TestOnMethods()
 		{
-			using (MessageSink.PushCurrent(_msgHolder)) {
-				TestEcs("void Set(set int X);", "void Set(set int X);"); // Body required
-				Assert.IsTrue(_msgHolder.List.Count > 0, "warning expected");
-			}
 			TestEcs("void Set(set int X, set int Y) {}",
 				"void Set(int x, int y) { X = x; Y = y; }");
 			TestEcs("void Set(public int X, bool Y, private string Z) { if (Y) Rejoice(); }",
@@ -24,6 +20,56 @@ namespace LeMP.Tests
 				void Set(int x, bool Y, string z) {
 					X = x; Z = z; if (Y) Rejoice();
 				}");
+		}
+
+		[Test]
+		public void TestCreateProperties()
+		{
+			TestEcs("void Prop(public Foo Foo {get; private set;}) {}", @"
+				public Foo Foo {get; private set;} 
+				void Prop(Foo foo) { Foo = foo; }");
+			TestEcs("void Prop(public Foo Foo {get; private set;}, int _bar {get;} = 0) {}", @"
+				public Foo Foo {get; private set;} 
+				int _bar {get;}
+				void Prop(Foo foo, int bar = 0) { Foo = foo; _bar = bar; }");
+		}
+
+		[Test]
+		public void TestOnConstructor()
+		{
+			TestEcs(@"class Point { 
+				public Point(public int X, public int Y) {}
+				public this(set int X, set int Y) {}
+			}", @"
+			class Point {
+				public int X;
+				public int Y;
+				public Point(int x, int y) { X = x; Y = y; }
+				public Point(int x, int y) { X = x; Y = y; }
+			}");
+
+			TestEcs(@"class Point { 
+				public Point(public int X, public int Y) : base(X + 1, Y + 1) {}
+			}", @"
+			class Point {
+				public int X;
+				public int Y;
+				public Point(int x, int y) : base(x + 1, y + 1) { X = x; Y = y; }
+			}");
+		}
+
+		[Test]
+		public void TestNoBodyError()
+		{
+			using (MessageSink.PushCurrent(_msgHolder)) {
+				TestEcs("void Set(set int X);", "void Set(set int X);"); // Body required
+				Assert.IsTrue(_msgHolder.List.Count > 0, "warning expected");
+			}
+		}
+
+		[Test]
+		public void TestAttributeTransfer()
+		{
 			TestEcs(
 				@"void Set(
 					[Spanish] set int _hola, 
@@ -52,33 +98,8 @@ namespace LeMP.Tests
 					_Bonjour = Bonjour;
 					Foo(_ciao);
 				}");
-			TestEcs(@"class Point { 
-				public Point(public int X, public int Y) {}
-				public this(set int X, set int Y) {}
-			}", @"
-			class Point {
-				public int X;
-				public int Y;
-				public Point(int x, int y) { X = x; Y = y; }
-				public Point(int x, int y) { X = x; Y = y; }
-			}");
 			TestEcs("void Set(public params string[] Strs) {}",
 				"public string[] Strs; void Set(params string[] strs) { Strs = strs; }");
-			TestEcs("void Prop(public Foo Foo {get; private set;}) {}", @"
-				public Foo Foo {get; private set;} 
-				void Prop(Foo foo) { Foo = foo; }");
-			TestEcs("void Prop(public Foo Foo {get; private set;}, int _bar {get;} = 0) {}", @"
-				public Foo Foo {get; private set;} 
-				int _bar {get;}
-				void Prop(Foo foo, int bar = 0) { Foo = foo; _bar = bar; }");
-			TestEcs(@"class Point { 
-				public Point(public int X, public int Y) : base(X + 1, Y + 1) {}
-			}", @"
-			class Point {
-				public int X;
-				public int Y;
-				public Point(int x, int y) : base(x + 1, y + 1) { X = x; Y = y; }
-			}");
 		}
 	}
 }
