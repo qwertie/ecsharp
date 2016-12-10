5abac698a7fb980a5dc6a350fd8c8f20cb630e42
David Piepgrass <qwertie256@gmail.com>
2016-12-11 06:32:31 +0800

    Re-run LeMP

---
+++ Core/Loyc.Essentials/Collections/ExtensionMethods/LinqToCollections.out.cs
@@ -0,0 +1,74 @@
+// Generated from LinqToCollections.ecs by LeMP custom tool. LeMP version: 2.3.1.0
+// Note: you can give command-line arguments to the tool via 'Custom Tool Namespace':
+// --no-out-header       Suppress this message
+// --verbose             Allow verbose messages (shown by VS as 'warnings')
+// --timeout=X           Abort processing thread after X seconds (default: 10)
+// --macros=FileName.dll Load macros from FileName.dll, path relative to this file 
+// Use #importMacros to use macros in a given namespace, e.g. #importMacros(Loyc.LLPG);
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Loyc.Collections
+{
+	/// <summary>
+	/// Work in progress. This class will enhance LINQ-to-Objects with 
+	/// type-preserving and/or higher-performance extension methods.
+	/// </summary><remarks>
+	/// For example, the <see cref="Enumerable.Last(IEnumerable{T})"/> extension 
+	/// method scans the entire list before returning the last item, while 
+	/// <see cref="Last(IReadOnlyList{T})"/> and <see cref="Last(IList{T})"/> simply
+	/// return the last item directly.
+	/// </remarks>
+	public static class LinqToCollections
+	{
+		public static int Count<T>(this IList<T> list)
+		{
+			return list.Count;
+		}
+		public static T Last<T>(this IList<T> list)
+		{
+			int last = list.Count - 1;
+			if (last < 0)
+				throw new EmptySequenceException();
+			return list[last];
+		}
+		public static T LastOrDefault<T>(this IList<T> list, T defaultValue = default(T))
+		{
+			int last = list.Count - 1;
+			return last < 0 ? defaultValue : list[last];
+		}
+		public static int Count<T>(this IReadOnlyList<T> list)
+		{
+			return list.Count;
+		}
+		public static T Last<T>(this IReadOnlyList<T> list)
+		{
+			int last = list.Count - 1;
+			if (last < 0)
+				throw new EmptySequenceException();
+			return list[last];
+		}
+		public static T LastOrDefault<T>(this IReadOnlyList<T> list, T defaultValue = default(T))
+		{
+			int last = list.Count - 1;
+			return last < 0 ? defaultValue : list[last];
+		}
+		public static IList<T> Skip<T>(this IList<T> list, int start)
+		{
+			return list.Slice(start);
+		}
+		public static IList<T> Take<T>(this IList<T> list, int count)
+		{
+			return list.Slice(0, count);
+		}
+		public static IListSource<T> Skip<T>(this IListSource<T> list, int start)
+		{
+			return list.Slice(start);
+		}
+		public static IListSource<T> Take<T>(this IListSource<T> list, int count)
+		{
+			return list.Slice(0, count);
+		}
+	}
+}
--- Core/Loyc.Syntax/LES/Les3Parser.out.cs
+++ Core/Loyc.Syntax/LES/Les3Parser.out.cs
@@ -1,4 +1,4 @@
-// Generated from Les3Parser.ecs by LeMP custom tool. LeMP version: 2.3.0.0
+// Generated from Les3Parser.ecs by LeMP custom tool. LeMP version: 2.3.1.0
 // Note: you can give command-line arguments to the tool via 'Custom Tool Namespace':
 // --no-out-header       Suppress this message
 // --verbose             Allow verbose messages (shown by VS as 'warnings')
@@ -567,8 +567,8 @@ namespace Loyc.Syntax.Les
 						}
 					case TT.Colon:
 						{
-							if ((TT) LA(0 + 1) != TT.Newline) {
-								if (CanParse(context, 0, out prec))
+							if (CanParse(context, 0, out prec)) {
+								if ((TT) LA(0 + 1) != TT.Newline)
 									goto matchExpr;
 								else
 									goto stop;
@@ -612,8 +612,8 @@ namespace Loyc.Syntax.Les
 						break;
 					case TT.LBrace:
 						{
-							if (context.CanParse(P.Primary)) {
-								if (_allowBlockCalls)
+							if (_allowBlockCalls) {
+								if (context.CanParse(P.Primary))
 									e = FinishPrimaryExpr(e);
 								else
 									goto stop;
