6c416d6ac75a8c2171b60c7b6341377cf5e32cec
David Piepgrass <qwertie256@gmail.com>
2018-09-29 09:51:11 -0600

    Tweak to auto-build Loyc.sln

--- UpdateLibLeMPAndReinstall.bat
+++ UpdateLibLeMPAndReinstall.bat
@@ -1,8 +1,9 @@
-REM This script builds the extension (vsix file) for Visual Studio 2015/2017,
-REM copies it to Lib/LeMP and then starts the vsix file. 
+REM This script builds the extension (vsix file) for Visual Studio 2017,
+REM copies it to Lib/LeMP and then starts the vsix file.
 REM   Note: Any previously installed vsix must first be uninstalled manually 
-REM from within VS, and you cannot use this script until after you 
-REM successfully build Loyc.sln in Release.NET45 mode.
+REM from within VS.
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" /out vsbuild.log /build Release.NET45 "Loyc.sln"
+@IF ERRORLEVEL 1 GOTO ERROR
 copy Bin\Release.NET45\*.dll Lib\LeMP
 @IF ERRORLEVEL 1 GOTO ERROR
 copy Bin\Release.NET45\*.exe Lib\LeMP
