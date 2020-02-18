481a1e88b9fbaee1e2df272c1e81c4be7eb35081
David Piepgrass <qwertie256@gmail.com>
2020-02-17 18:05:43 -0700

    v2.7.0.2: Set up Loyc.Interfaces NuGet package

--- Core/AssemblyVersion.cs
+++ Core/AssemblyVersion.cs
@@ -14,5 +14,5 @@ using System.Reflection;
 // command to change the version number which, I guess, produces an incompatible 
 // assembly in the presence of strong names (strong naming prevents two assemblies 
 // from linking together without an exact match.)
-[assembly: AssemblyVersion("2.7.0.1")]
-[assembly: AssemblyFileVersion("2.7.0.1")]
+[assembly: AssemblyVersion("2.7.0.2")]
+[assembly: AssemblyFileVersion("2.7.0.2")]
