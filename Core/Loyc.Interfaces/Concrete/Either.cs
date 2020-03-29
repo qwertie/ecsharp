c390fb10a7068b6a6e41575cccc62e3e185d4144
David Piepgrass <qwertie256@gmail.com>
2020-03-29 17:36:04 -0600

    v2.7.1.2: Oops. Forgot `Either<,>` in .NET4 version.

--- Core/Loyc.Interfaces/Concrete/Either.cs
+++ Core/Loyc.Interfaces/Concrete/Either.cs
@@ -9,7 +9,7 @@ namespace Loyc.Concrete
 	/// <summary>Holds a single value of one of two types (L or R).</summary>
 	/// <remarks>For efficiency, this is a struct, but this makes it possible
 	/// to default-construct it. In that case its value will be <c>default(R)</c>.</remarks>
-	public struct Either<L, R> : IEither<L, R>
+	public struct Either<L, R> : IEither<L, R>, IHasValue<object>
 	{
 		/// <summary>Simply calls the constructor. This method exists to make
 		/// it possible to construct an Either when both types are the same.</summary>
@@ -36,6 +36,7 @@ namespace Loyc.Concrete
 		private readonly bool _hasLeft;
 		public Maybe<L> Left => _hasLeft ? _left : new Maybe<L>();
 		public Maybe<R> Right => _hasLeft ? new Maybe<R>() : _right;
+		public object Value => _hasLeft ? (object) _left : _right;
 
 		public static implicit operator Either<L, R>(L value) => NewLeft(value);
 		public static implicit operator Either<L, R>(R value) => NewRight(value);
