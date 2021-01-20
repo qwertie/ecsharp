409b357029f1e7b544647857401fe644a7b84e87
David Piepgrass <qwertie256@gmail.com>
2021-01-20 06:33:49 -0700

    Fix the build some more

--- Core/Loyc.Interfaces/Collections/ITuple.cs
+++ Core/Loyc.Interfaces/Collections/ITuple.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 
 namespace Loyc.Compatibility
 {
-	#if NetStandard20 || DotNet45
+#if NetStandard20 || DotNet45
 
 	/// <summary>Defines ITuple in .NET Standard 2.0. System.Runtime.CompilerServices.ITuple
 	/// was added to .NET Core 2.0 and .NET Framework 4.7.1, but it wasn't added to .NET 
@@ -12,6 +12,11 @@ namespace Loyc.Compatibility
 		object this[int index] { get; }
 		int Length { get; }
 	}
+
+	#else
+
+	/// <summary>Ensure `using Loyc.Compatibility` is not an error</summary>
+	public interface __ThisEnsuresTheNamespaceIsNotEmpty { }
 
 	#endif
 }
