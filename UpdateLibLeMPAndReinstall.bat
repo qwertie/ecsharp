a88517540806516f7e43fef60dbd7b486f96026a
David Piepgrass <qwertie256@gmail.com>
2024-06-30 10:34:09 -0600

    v30.1.2: minimal changes to update EC#/LeMP/LLLPG extension for VS2022

--- UpdateLibLeMPAndReinstall.bat
+++ UpdateLibLeMPAndReinstall.bat
@@ -1,8 +1,8 @@
-REM This script builds the extension (vsix file) for Visual Studio 2017,
+REM This script builds the extension (vsix file) for Visual Studio 2022,
 REM copies it to Lib/LeMP and then starts the vsix file.
 REM   Note: Any previously installed vsix must first be uninstalled manually 
 REM from within VS.
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" /out vsbuild.log /build Release "Loyc.netfx.sln"
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe" /out vsbuild.log /build Release "Loyc.netfx.sln"
 @IF ERRORLEVEL 1 GOTO ERROR
 mkdir Lib\LeMP
 copy Bin\Release\*.dll Lib\LeMP
@@ -13,11 +13,11 @@ copy Bin\Release\*.xml Lib\LeMP
 copy Bin\Release\*.pdb Lib\LeMP
 copy Bin\Release\*.exe.config Lib\LeMP
 
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" /out vsbuild.log /build Debug "Visual Studio Integration\Visual Studio Integration 2017.sln"
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe" /out vsbuild.log /build Debug "Visual Studio Integration\Visual Studio Integration 2022.sln"
 @echo off
 @IF ERRORLEVEL 1 GOTO ERROR
 
-copy "Visual Studio Integration\LoycForVS2017\bin\Debug\LeMP_VisualStudio.vsix" "Lib\LeMP"
+copy "Visual Studio Integration\LoycForVS2022\bin\Debug\LeMP_VisualStudio.vsix" "Lib\LeMP"
 @IF ERRORLEVEL 1 GOTO ERROR
 
 "Lib\LeMP\LeMP_VisualStudio.vsix"
